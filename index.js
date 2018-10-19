055cf9c11bc00804f3f480cde7dc00fb508b4a7d
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-19 13:10:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ function whisper(string) {
 }
 
 function logShout( string){
+<<<<<<< HEAD
   return console.log(string.toUpperCase()) 
+=======
+  spy = string
+  return console.log(string.toUpperCase) 
+>>>>>>> 9b873a7cc29b5ee20f3f6c67d843871247490c1b
 
 }
 
 function logWhisper(string){
+<<<<<<< HEAD
   console.log(string.toLowerCase())
+=======
+  spy.HELLO
+  console.log(string.toLowerCase)
+>>>>>>> 9b873a7cc29b5ee20f3f6c67d843871247490c1b
 }
 
 function sayHiToGrandma(string){
